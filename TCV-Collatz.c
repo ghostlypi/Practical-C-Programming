257e7aac58c36b31ee0bcf625f0813839533f207
PI <main@parthiyer.com>
2020-09-24 20:28:28 -0700

    Update

--- TCV-Collatz.c
+++ TCV-Collatz.c
@@ -13,54 +13,44 @@ unsigned short collatz(int val){
     }
 }
 
-int* maxcollatz(int cap){
-    int max = 0;
-    int maxi = 0;
-    for(int i = 3; i < cap; i+=2){
-        int value = collatz(i);
-        if (value > max){
-            max = value;
-        }
-        if(max == value){
-            maxi = i;
-        }
-    }
-    int* out = malloc(2*sizeof(int));
-    out[1] = max;
-    out[0] = maxi;
-    return out;
-}
-
 double* maxcollatzratio(int cap){
     int max = 0;
     int maxi = 0;
-    double maxr = 0;
+    int rmax = 0;
+    int rmaxi = 0;
+    double rmaxr = 0;
     for (int i = 3; i < cap; i += 2){
         int value = collatz(i);
         double ratio = ((double)value/i);
-        if(ratio > maxr){
-            maxr = ratio;
-        }
-        if (maxr == ratio){
-            maxi =i;
-        }
-        if (maxr == ratio){
+        if (value > max){
             max = value;
         }
+        if (max == value){
+            maxi = i;
+        }
+        if(ratio > rmaxr){
+            rmaxr = ratio;
+        }
+        if (rmaxr == ratio){
+            rmaxi =i;
+        }
+        if (rmaxr == ratio){
+            rmax = value;
+        }
     }
-    double* out = malloc(3*sizeof(double));
-    out[2] = maxr;
+    double* out = malloc(5*sizeof(double));
+    out[4] = rmaxr;
+    out[3] = rmax;
+    out[2] = rmaxi;
     out[1] = max;
     out[0] = maxi;
     return out;
 }
 
 int main(int argc, char* argv){
-    int* arr = maxcollatz(100000);
     double* arr2 = maxcollatzratio(100000);
-    printf("%d has a stopping time of %d\n", arr[0], arr[1]);
-    printf("%.0lf has a stopping time of %.0lf and a ratio of %.2lf\n",arr2[0],arr2[1],arr2[2]);
-    free(arr);
+    printf("%d has a stopping time of %d\n", arr2[0], arr2[1]);
+    printf("%.0lf has a stopping time of %.0lf and a ratio of %.2lf\n",arr2[2],arr2[3],arr2[4]);
     free(arr2);
     return 0;
 }
