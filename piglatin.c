5bf3ff538b7a38a66c73c628d71d97bc0f7d087b
PI <main@parthiyer.com>
2020-11-12 11:10:56 -0800

    update

--- piglatin.c
+++ piglatin.c
@@ -8,24 +8,8 @@ bool isCapped(const char c) {
   return (c < 91) && (c>=65);
 }
 
-char toLower(const char c){
-  if (c < 91 && c > 64){
-    return c + 32;
-  }else{
-    return c;
-  }
-}
-
-char toUpper(const char c){
-  if (c > 96 && c < 123){
-    return c + 32;
-  }else{
-    return c;
-  }
-}
-
 bool isVowel(const char chr) {
-  char c = toLower(chr);
+  char c = tolower(chr);
   return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
 }
 
@@ -35,49 +19,38 @@ char *word_to_pig_latin(const char *word) {
     size_t len = strlen(word)+4;
     char* out = malloc(sizeof(char)*len);
     if (capped){
-      out[0] = toUpper(word[0]);
+      out[0] = toupper(word[0]);
     }else{
       out[0] = word[0];
     }
     int i = 1;
-    for(i < strlen(word); i++;){
-      printf("%c",word[i]);
+    for(; i < strlen(word); i++){
       out[i] = word[i];
     }
-    out[i]='w';
-    out[i+1]='a';
-    out[i+2]='y';
-    out[i+3]=0;
+    strcat(out,"way");
     return out;
   }else{
     size_t len = strlen(word)+3;
-    char* end = malloc(sizeof(char)*len);
     char* out = malloc(sizeof(char)*len);
-    int i = 0;
-    while((i < len-2) && !isVowel(word[i])){
-      end[i] = word[i];
-      i++;
-    }
-    end[i+1]='a';
-    end[i+2]='y';
-    end[i+3]=0;
+    char end[len];
+    memset(end,0,len*sizeof(char));
+    int start = strcspn(word,"aeiouy");
+    int i = strcspn(word,"aeiouy");
     if(capped){
-      out[0] = toUpper(word[i]);
+      out[0] = toupper(word[i]);
     }else{
       out[0] = word[i];
     }
     i++;
-    int j;
-    for(j = 1; i < len-2; i++){
+    for(int j = 1; i < len-2; i++){
       out[j] = word[i];
       j++;
     }
-    i = 0;
-    while(end[i]!=0){
-      out[j] = end[i];
-      i++;
-      j++;
+    for(i = 0; i<start; i++){
+      end[i] = tolower(word[i]);
     }
+    strcat(out,end);
+    strcat(out,"ay");
     return out;
   }
 }
